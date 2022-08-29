268c69f7584ae15c08a71e784e1b8449d6d62fd8
Ngowwa-Ziro <joseph.ngowwa@student.moringaschool.com>
2022-08-29 08:02:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers,argument){
+    return drivers.filter(driver => driver.toLowerCase() === argument.toLowerCase())
+}
+
+function fuzzyMatch(drivers,argument){
+    return drivers.filter(driver => driver.startsWith(argument))
+}
+
+function matchName(drivers,argument){
+    return drivers.filter(driver => driver.name === argument)
+}
